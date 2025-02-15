89afaf021374696a28f21315e14b195a012041ce
ratiebareeng <bareengbiz@gmail.com>
2025-02-15 20:34:21 +0200

    Add scrolling

--- lib/error_page.dart
+++ lib/error_page.dart
@@ -18,30 +18,32 @@ class SimpleErrorPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Oops! Something went wrong.',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Error: $errorMessage',
-              style: const TextStyle(fontSize: 16),
-            ),
-            if (stackTrace != null) ...[
-              const SizedBox(height: 16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
               const Text(
-                'Stack Trace:',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                'Oops! Something went wrong.',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
+              const SizedBox(height: 16),
               Text(
-                stackTrace.toString(),
-                style: const TextStyle(fontSize: 14),
+                'Error: $errorMessage',
+                style: const TextStyle(fontSize: 16),
               ),
+              if (stackTrace != null) ...[
+                const SizedBox(height: 16),
+                const Text(
+                  'Stack Trace:',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  stackTrace.toString(),
+                  style: const TextStyle(fontSize: 14),
+                ),
+              ],
             ],
-          ],
+          ),
         ),
       ),
     );
